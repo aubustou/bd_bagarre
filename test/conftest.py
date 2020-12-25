39a6e9b0a93de2fb0bf9163447649e16e3039531
aubustou <survivalfr@yahoo.fr>
2020-12-25 22:43:39 +0100

    Fix Pytest errors

--- test/conftest.py
+++ test/conftest.py
@@ -1,13 +1,12 @@
 import pytest
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, close_all_sessions
 from bd_bagarre.database import Base
 
 
 @pytest.fixture(scope="function", autouse=True)
 def db_session():
     engine = create_engine("sqlite:///database.sqlite3", echo=True)
-    session = scoped_session(sessionmaker(autocommit=False, bind=engine))
     yield
-    session.close_all_sessions()
+    close_all_sessions()
     Base.metadata.drop_all(bind=engine)
